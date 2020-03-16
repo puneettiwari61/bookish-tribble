191453ee59693a5fa5ad24d48d99f6ceb5db22cc
Puneet <puneettiwari61@gmail.com>
2020-03-16 23:56:33 +0530

    post

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,12 @@ router.get('/', function(req, res, next) {
   console.log('users route', req.headers.host)
   
   res.send('respond with a resource' + req.headers.host );
+  // curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
 });
 
+
+router.post('/', function(req,res,next) {
+  console.log('its a post')
+  res.json({success: true})
+})
 module.exports = router;
