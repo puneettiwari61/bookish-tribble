b7e8a1873308dd899a9172fe8aab161a09a406c9
Puneet <puneettiwari61@gmail.com>
2020-03-16 23:21:19 +0530

    initial4

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   console.log('users route')
-  res.send('respond with a resource' + req);
+  res.send('respond with a resource' + JSON.stringify(req));
 });
 
 module.exports = router;
