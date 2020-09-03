14f509db787ab9fc7982e50d389655a038b22960
Puneet Tiwari <puneettiwari61@gmail.com>
2020-09-03 21:46:42 +0530

    Update users.js
    update

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log('users route', req.headers.host)
+//   console.log('users route', req.headers.host)
+  console.log('req.body', req.body)
   
   res.send('respond with a resource' + req.headers.host );
   // curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
